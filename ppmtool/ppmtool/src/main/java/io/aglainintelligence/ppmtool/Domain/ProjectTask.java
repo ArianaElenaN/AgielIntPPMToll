d9864762ad3622eedf4da3648b89756994cbf6ac
Ariana Neagu <ariana_neagu@yahoo.com>
2022-08-09 15:20:24 +0300

    5th commit

---
+++ ppmtool/ppmtool/src/main/java/io/aglainintelligence/ppmtool/Domain/ProjectTask.java
@@ -0,0 +1,2 @@
+package io.aglainintelligence.ppmtool.Domain;public class ProjectTask {
+}
---
+++ ppmtool/ppmtool/src/main/java/io/aglainintelligence/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,2 @@
+package io.aglainintelligence.ppmtool.repositories;public interface BacklogRepository {
+}
